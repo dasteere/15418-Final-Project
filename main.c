29065846385328743c02fd440d51bfab03e00ece
David A Steere <dsteere@GHC44.GHC.ANDREW.CMU.EDU>
2017-05-10 16:58:28 -0400

    compile issues

--- main.c
+++ main.c
@@ -6,27 +6,40 @@
 
 int main() {
     int oopSize, ipSize, potSize, betSize;
-    char rank1, suit1, rank2, suit2;
+    char card1[2], card2[2];
     scanf("%d %d %d %d\n", oopSize, ipSize, potSize, betSize);
     card_t *oopRange = (card_t *) malloc(2 * oopSize * sizeof(card_t));
     card_t *ipRange = (card_t *) malloc(2 * ipSize * sizeof(card_t));
     card_t board[HAND_SIZE];
     for (int i = 0; i < HAND_SIZE; i++) {
         if (i > 0) scanf(" ");
-        scanf("%c%c", rank1, rank1);
-        board[i] = str_to_card(rank1, suit1);
+        scanf("%c%c", card1, card1 + 1);
+        if (str_to_card(card1, board[i]) < 0) {
+            printf("Invalid input: %c%c\n", card1, card1+1);
+            return 0;
+        }
     }
-    scanf("\n");
+    scanf("\nOOP RANGE\n");
     for (int i = 0; i < oopSize; i++) {
-        scanf("%c%c %c%c\n", rank1, suit1, rank2, suit2);
-        oopRange[i*2] = str_to_card(rank1, suit1);
-        oopRange[(i*2) + 1] = str_to_card(rank2, suit2);
+        scanf("%c%c %c%c\n", card1, card1 + 1, card2, card2);
+        if (str_to_card(card1, oopRange[i*2]) < 0) {
+            printf("Invalid input: %c%c\n", card1, card1+1);
+            return 0;
+        }
+        if (str_to_card(card2, oopRange[(i*2) + 1]) < 0) {
+            printf("Invlaid input: %c%c\n", card2, card2+1);
+        }
     }
-
+    scanf("IP RANGE\n")
     for (int i = 0; i < ipSize; i++) {
-        scanf("%c%c %c%c\n", rank1, suit1, rank2, suit2);
-        ipRange[i*2] = str_to_card(rank1, suit1);
-        ipRange[(i*2) + 1] = str_to_card(rank2, suit2);
+        scanf("%c%c %c%c\n", card1, card1 + 1, card2, card2 + 1);
+        if (str_to_card(card1, ipRange[i*2]) < 0) {
+            printf("Invalid input: %c%c\n", card1, card1+1);
+            return 0;
+        }
+        if (str_to_card(card2, ipRange[(i*2) + 1]) < 0) {
+            printf("Invlaid input: %c%c\n", card2, card2+1);
+        }
     }
 
     GlobalConstants *params = calcGlobalConsts(board, oopRange, oopSize, ipRange, ipSize, potSize, betSize);
