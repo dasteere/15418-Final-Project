3e0c05ddb3be0167db51c613053e538b039b6522
David A Steere <dsteere@GHC44.GHC.ANDREW.CMU.EDU>
2017-05-10 17:28:35 -0400

    cahnges

--- main.c
+++ main.c
@@ -10,28 +10,29 @@ int main() {
     scanf("%d %d %d %d\n", &oopSize, &ipSize, &potSize, &betSize);
     card_t *oopRange = (card_t *) malloc(2 * oopSize * sizeof(card_t));
     card_t *ipRange = (card_t *) malloc(2 * ipSize * sizeof(card_t));
-    card_t board[HAND_SIZE];
-    for (int i = 0; i < HAND_SIZE; i++) {
-        printf("%d\n", i);
+    card_t board[BOARD_SIZE];
+    for (int i = 0; i < BOARD_SIZE; i++) {
         scanf("%c%c,", card1, card1 +1);
-        printf("Read in %c%c\n", card1[0], card1[1]);
         if (str_to_card(card1, &board[i]) < 0) {
             printf("Invalid input: %c%c\n", card1[0], card1[1]);
             return 0;
         }
     }
-    scanf("\nOOP RANGE\n");
+    scanf("\n---OOP RANGE---\n");
+    printf("Read in board\n");
     for (int i = 0; i < oopSize; i++) {
-        scanf("%c%c,%c%c,\n", card1, card1 + 1, card2, card2);
+        scanf("%c%c,%c%c,\n", card1, card1 + 1, card2, card2 + 1);
         if (str_to_card(card1, &oopRange[i*2]) < 0) {
             printf("Invalid input: %c%c\n", card1[0], card1[1]);
             return 0;
         }
         if (str_to_card(card2, &oopRange[(i*2) + 1]) < 0) {
-            printf("Invlaid input: %c%c\n", card2[0], card2[1]);
+            printf("Invalid input: %c%c\n", card2[0], card2[1]);
+            return 0;
         }
     }
-    scanf("IP RANGE\n");
+    scanf("---IP RANGE---\n");
+    printf("Read in OOP\n");
     for (int i = 0; i < ipSize; i++) {
         scanf("%c%c,%c%c,\n", card1, card1 + 1, card2, card2 + 1);
         if (str_to_card(card1, &ipRange[i*2]) < 0) {
@@ -39,10 +40,11 @@ int main() {
             return 0;
         }
         if (str_to_card(card2, &ipRange[(i*2) + 1]) < 0) {
-            printf("Invlaid input: %c%c\n", card2[0], card2[1]);
+            printf("Invalid input: %c%c\n", card2[0], card2[1]);
+            return 0;
         }
     }
-
+    printf("Read in IP\n");
     GlobalConstants *params = calcGlobalConsts(board, oopRange, oopSize, ipRange, ipSize, potSize, betSize);
 
     char *bestOopStrategy = (char *) malloc(oopSize * sizeof(char));
